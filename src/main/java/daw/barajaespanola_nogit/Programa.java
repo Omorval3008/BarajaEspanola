173b10f5b736e14c860d7119c9800be0dc05244c
Omorval3008 <omorval3008@g.educaand.es>
2023-12-01 09:53:46 +0100

    C0-Inicio programa

---
+++ src/main/java/daw/barajaespanola_nogit/Programa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw.barajaespanola_nogit;
+
+/**
+ *
+ * @author oscar
+ */
+public class Programa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
